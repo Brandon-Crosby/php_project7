d546b453538036d55a7ed3a08d5e9641bf0812e9
Brandon-Crosby <53662951+Brandon-Crosby@users.noreply.github.com>
2020-03-06 02:03:14 -0500

    Add files via upload

--- src/routes.php
+++ src/routes.php
@@ -12,45 +12,45 @@ $app->get('/', function ($request, $response, $args) {
     ];
     $result = [
       'endpoints' => $endpoints,
-      'version' => $this->$api['version'],
+      'version' => $this->api['version'],
       'timestamp' => time(),
     ];
-    return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+    return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
-$app->group('/api/v1/tasks', function() use($app) {
+$app->group('/api/v1/todos', function() use($app) {
     $app->get('', function (Request $request, Response $response, array $args) {
       $result = $this->task->getTasks();
-      return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+      return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 $app->get('/{task_id}', function (Request $request, Response $response, array $args) {
-      $result = $this->task->getTasks($args['task_id']);
-      return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+      $result = $this->task->getTask($args['task_id']);
+      return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 $app->post('', function (Request $request, Response $response, array $args) {
-      $result = $this->task->createTasks($args['task_id']);
-      return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+      $result = $this->task->createTask($args['task_id']);
+      return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 $app->put('/{task_id}', function (Request $request, Response $response, array $args) {
-      $result = $this->task->createTasks($args['task_id']);
+      $result = $this->task->updateTask($args['task_id']);
       $data = $request->getParsedBody();
       $data['task_id'] = $args['task_id'];
       $result = $this->task->updateTask($data);
-      return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+      return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 $app->delete('/{task_id}', function (Request $request, Response $response, array $args) {
-      $result = $this->task->deleteTasks($args['task_id']);
-      return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+      $result = $this->task->deleteTask($args['task_id']);
+      return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 });
 /*
 $app->group('/{task_id}/reviews', function () use ($app) {
           $app->get('', function (Request $request, Response $response, array $args) {
               $result = $this->review->getReviewsBytaskId($args['task_id']);
-              return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+              return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     });
 $app->get('/{review_id}', function (Request $request, Response $response, array $args) {
               $result = $this->review->getReview($args['review_id']);
-              return $response->withJson($results, 200, JSON_PRETTY_PRINT);
+              return $response->withJson($result, 200, JSON_PRETTY_PRINT);
       });
 });
 });
